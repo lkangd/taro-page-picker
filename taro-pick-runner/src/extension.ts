0e210925a8b9e526bc7315aeade76d152c4ccfcc
CurtisLiong <lkangd@gmail.com>
2021-05-11 08:54:18 +0800

    init

---
+++ taro-pick-runner/src/extension.ts
@@ -0,0 +1,58 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+const fs = require('fs');
+const path = require('path');
+const babel = require('@babel/core');
+import * as json from 'jsonc-parser';
+
+import { PickView } from './pickView';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "taro-pick-runner" is now active!');
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand('taro-pick-runner.helloWorld', () => {
+    // The code you place here will be executed every time your command is executed
+
+    // Display a message box to the user
+    vscode.window.showInformationMessage('Hello World from TaroPickRunner! This is the first step.');
+  });
+
+  context.subscriptions.push(disposable);
+
+  disposable = vscode.commands.registerCommand('taro-pick-runner.showTime', () => {
+    // The code you place here will be executed every time your command is executed
+
+    // Display a message box to the user
+    const code = fs.readFileSync(path.resolve(vscode.workspace.workspaceFolders?.[0]?.uri.fsPath, 'src/app.tsx'), { flag: 'r+', encoding: 'utf8' });
+    try {
+			console.log('code', code)
+			const tree = json.parseTree(code)
+			console.log('tree', tree)
+      // const script = babel.transformSync('code()', {
+        // presets: ['@babel/preset-react', '@babel/preset-typescript'],
+        // plugins: [require.resolve('@babel/plugin-proposal-class-properties'), require.resolve('@babel/plugin-proposal-object-rest-spread')],
+      // });
+      // const fsPath = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath
+      // script && console.log(script);
+      // fs.writeFileSync(path.resolve(vscode.workspace.workspaceFolders?.[0]?.uri.fsPath, 'src/app2.tsx'), script.code);
+    } catch (e) {
+      console.log(e);
+    }
+    vscode.window.showWarningMessage(`Current Time is: ${JSON.stringify(vscode.workspace.workspaceFolders)}`);
+  });
+
+  context.subscriptions.push(disposable);
+
+  new PickView(context);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
