630785e003fa3b96606869a5279817391b3d7673
CurtisLiong <lkangd@gmail.com>
2021-06-05 19:56:26 +0800

    feat(src/pick_view.ts): 增加是否只显示已选页面功能,增加全页面的已选/总页面

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,6 +10,9 @@ import * as path from 'path'
 // const
 import { TPP_STORAGE_NAME, TPP_SUFFIX_TYPE } from '../const'
 
+// type
+import { TreeItemRoot, TreeItemSubPackage, TreeItemPage } from '../type'
+
 export const entryToVscodeDir = (appEntry: string) => {
   const appEntrySeparate = appEntry.split(path.sep)
   const retAppEntry = appEntrySeparate
@@ -22,4 +25,23 @@ export const entryToVscodeDir = (appEntry: string) => {
 
 export const findPagePath = (fsPath: string): string | undefined => {
   return TPP_SUFFIX_TYPE.map(ext => `${fsPath}.${ext}`).find(ext => fs.existsSync(ext))
+}
+
+export const calAPCount = (root: TreeItemRoot): { all: number; picked: number } => {
+  let all = 0
+  let picked = 0
+  // main pages
+  for (const page of root.pages) {
+    all++
+    page.picked && (picked++)
+  }
+  // sub packages pages
+  for (const subPackage of (root.subPackages || [])) {
+    for (const page of subPackage.pages) {
+      all++
+      page.picked && (picked++)
+    }
+  }
+
+  return { all, picked }
 }
